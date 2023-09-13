67662846257b3558b6cbf339adca7442ac3bf554
seongs <51193872+seongs@users.noreply.github.com>
2023-09-13 16:00:16 +0900

    [unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
@@ -1,5 +1,5 @@
-def solution(start, end):
+def solution(start_num, end_num):
     answer = []
-    for i in range(start,end-1,-1):
+    for i in range(start_num,end_num-1,-1):
         answer.append(i)
     return answer
