e43aff708d4a4fa37a3942b557b6a4f88bfb25cc
seongs <epik1859@gmail.com>
2024-07-03 17:51:04 +0900

    [level 0] Title: 짝수의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,6 +1,2 @@
 def solution(n):
-    answer =[]
-    for i in range(n+1):
-        if i % 2 == 0:
-            answer.append(i)
-    return sum(answer)
+    return sum([i for i in range(2,n+1,2) ])
