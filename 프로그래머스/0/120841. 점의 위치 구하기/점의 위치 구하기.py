b14397b0f051e67dae7adc7c01f08fd2955f904f
seongs <epik1859@gmail.com>
2024-07-11 10:01:34 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,5 +1,3 @@
 def solution(dot):
-    if dot[0] > 0 and dot[1] > 0 : return 1
-    elif dot[0] < 0 and dot[1] > 0 : return 2
-    elif dot[0] < 0 and dot[1] < 0 : return 3
-    elif dot[0] > 0 and dot[1] < 0 : return 4
+    quad = [(3,2),(4,1)]
+    return quad[dot[0] > 0][dot[1] > 0]
