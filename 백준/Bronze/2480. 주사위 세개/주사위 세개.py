13f0a964703a0355edc1377a8a6736477efe574c
seongs <epik1859@gmail.com>
2025-08-19 17:49:42 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,6 @@
+a = list(map(int, input().split()))
+
+if   a[0] == a[1] and a[0] == a[2] : print(10000+a[0]*1000) 
+elif a[0] == a[1] or  a[1] == a[2] : print(1000+a[1]*100)
+elif a[2] == a[0] or  a[2] == a[1] : print(1000+a[2]*100)
+else                               : print(max(a)*100)
