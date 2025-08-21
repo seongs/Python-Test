da36d5e46fbb7c0412501775575f23df6d1b7646
seongs <epik1859@gmail.com>
2025-08-21 15:41:34 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,10 @@
+import sys
+N,M = map(int,sys.stdin.readline().split())
+basket = [0] * N
+for _ in range(M):
+    i,j,k = map(int,sys.stdin.readline().split())
+    for t in range(i-1,j):
+        basket[t] = k
+
+
+print(" ".join(map(str,basket)))
