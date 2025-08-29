8f6ba675ee733977b9faa1224eb836de60fd6d2a
seongs <epik1859@gmail.com>
2025-08-29 14:02:40 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+cnt = N
+for _ in range(N):
+    word = sys.stdin.readline().strip()
+    for j in range(0,len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
